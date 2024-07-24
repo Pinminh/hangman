7e71fcd35fbb0214a23a9d42cf627cb2c3af06f5
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-07-24 23:24:35 +0700

    refactor: migrate save game methods to MenuHandler
    Turn out save game functionality is also needed in end game menu, hence
    pause menu can't keep this save function for itself. Save game is now a
    part of MenuSignalHandler.

--- lib/cli/menu/pause_menu.rb
+++ lib/cli/menu/pause_menu.rb
@@ -15,7 +15,7 @@ class PauseMenu
     prompt = Rainbow('You have paused the game').bold.bright.gold
     choice = CLI::UI::Prompt.ask(prompt, filter_ui: false) do |handler|
       handler.option('Unpause') { :unpause }
-      handler.option('Save Game') { :save_game }
+      handler.option('Save Game') { :save_game_on_pause }
       handler.option('Restart') { :new_game }
       handler.option('Start Menu') { :start_menu }
     end
@@ -35,41 +35,5 @@ class PauseMenu
 
     cli.main_displayer.display
     display
-  end
-
-  private
-
-  def ask_save_name
-    name = ''
-    loop do
-      cli.main_displayer.display
-
-      prompt = Rainbow('Enter save name:').bright.gold
-      name = CLI::UI::Prompt.ask(prompt).strip
-
-      if name.length > 16
-        error = Rainbow('Name cannot exceed 16 characters').bright.red
-        CLI::UI::Printer.puts error
-        sleep 2
-      end
-
-      break if name.length <= 16
-    end
-    name
-  end
-
-  def display_save_window
-    save_title = Rainbow('Save Game').bold.bright.gold
-    CLI::UI::Frame.open(save_title, color: :yellow)
-
-    spin_init = Rainbow('Saving current game...').bright.yellow
-    spin_end = Rainbow('Complete successfully').bright.green
-    CLI::UI::Spinner.spin(spin_init) do |spinner|
-      sleep 1.0
-      spinner.update_title(spin_end)
-    end
-    sleep 1.0
-
-    CLI::UI::Frame.close nil
   end
 end
